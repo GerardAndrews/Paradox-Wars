7bd52a1bf1a8ad1905de00f91c467f92da9220d6
Gerard Andrews <gerard.andrews1@marist.edu>
2019-07-16 19:03:23 -0400

    Added Final Scripts to Repository

---
+++ Soundtrack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundtrack : MonoBehaviour {
+    public AudioSource soundtrack;
+    public AudioClip medAgeAS;
+    public AudioClip renAgeAS;
+
+    GameObject gameController;
+
+    // Use this for initialization
+    void Start () {
+		gameController = GameObject.FindGameObjectWithTag("GameController");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (gameController.GetComponent<Building_Conditions>().ageLevel == 2)
+        {
+            soundtrack.Pause();
+            soundtrack.clip = medAgeAS;
+            soundtrack.Play();
+        }
+
+        if (gameController.GetComponent<Building_Conditions>().ageLevel == 3)
+        {
+            soundtrack.Pause();
+            soundtrack.clip = renAgeAS;
+            soundtrack.Play();
+        }
+
+    }
+}
