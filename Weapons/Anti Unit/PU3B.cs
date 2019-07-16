454d27466e413ce236b2988adfe6dddabe59ac95
Gerard Andrews <gerard.andrews1@marist.edu>
2019-07-16 19:02:23 -0400

    Added Weapon Audio Scripts

---
+++ Weapons/Anti Unit/PU3B.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PU3B : MonoBehaviour
+{
+
+    //List our death audio here!
+    public AudioSource attack;
+    public AudioSource impact;
+
+
+    //List our Projectile list here:
+    GameObject[] projectileList;
+    int prevCount;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        projectileList = GameObject.FindGameObjectsWithTag("PU3Bullet");
+        prevCount = projectileList.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        projectileList = GameObject.FindGameObjectsWithTag("PU3Bullet");
+
+        // If the list goes down by 1
+        if (projectileList.Length < prevCount)
+        {
+            // Play a sound
+            PlayImpact();
+            prevCount = projectileList.Length;
+        }
+
+        if (projectileList.Length > prevCount)
+        {
+            prevCount = projectileList.Length;
+        }
+
+        // If the list goes up by 1
+        if (projectileList.Length > prevCount)
+        {
+            // Play a sound
+            PlayAttack();
+            prevCount = projectileList.Length;
+        }
+
+        if (projectileList.Length > prevCount)
+        {
+            prevCount = projectileList.Length;
+        }
+
+    }
+
+    void PlayAttack()
+    {
+        attack.Play();
+    }
+
+    void PlayImpact()
+    {
+        impact.Play();
+    }
+}
